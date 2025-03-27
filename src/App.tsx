3189fa85bd430d8dc00c555f00f4d48ec1d6e979
kami2693 <kamikaze2693@gmail.com>
2025-03-27 08:49:04 +0100

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -44,7 +44,6 @@ export const App = () => {
     async (todo: Todo) => {
       if (selectedTodo?.id === todo.id) {
         handleCloseModal();
-
         return;
       }
 
@@ -52,13 +51,12 @@ export const App = () => {
       setModalLoading(true);
       try {
         const user = await getUser(todo.userId);
-
         setSelectedUser(user);
       } finally {
         setModalLoading(false);
       }
     },
-    [handleCloseModal],
+    [handleCloseModal, selectedTodo?.id],
   );
 
   const filteredTodos = todos.filter(todo => {
